bd4609bfcb06891cccad7ec8045718a29b834f3f
K.P.S. Randula <87770007+Sahani99@users.noreply.github.com>
2026-05-10 01:05:08 +0530

    chore: scaffold NestJS backend app

---
+++ apps/backend/src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD, APP_FILTER } from '@nestjs/core';
+import { PrismaModule } from '../prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { LeadsModule } from './leads/leads.module';
+import { NotesModule } from './notes/notes.module';
+import { FilesModule } from './files/files.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { AllExceptionsFilter } from './common/filters/http-exception.filter';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ThrottlerModule.forRoot([{ ttl: 60000, limit: 100 }]),
+    PrismaModule,
+    AuthModule,
+    UsersModule,
+    LeadsModule,
+    NotesModule,
+    FilesModule,
+    DashboardModule,
+  ],
+  providers: [
+    { provide: APP_GUARD, useClass: ThrottlerGuard },
+    { provide: APP_FILTER, useClass: AllExceptionsFilter },
+  ],
+})
+export class AppModule {}
